94f7a0fcf1cfae750c865a80680a5af4efbf7d16
Maxim Kerkula <87918161+MaxKerkula@users.noreply.github.com>
2023-06-09 02:49:15 -0400

    Update JdbcTransferDao.java
    Removed redundant logic and methods

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,18 +1,94 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface TransferDAO {
+@Component
+public class JdbcTransferDAO implements TransferDAO {
 
-    List<Transfer> getAllTransfers(Integer userId);
+    private JdbcTemplate jdbcTemplate;
 
-    Transfer getTransferById(Integer userId, Integer transferId);
+    public JdbcTransferDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-    Transfer createTransfer(Integer userId, Transfer transfer);
+    @Override
+    public List<Transfer> getAllTransfers(Integer userId) {
+        List<Transfer> transfers = new ArrayList<>();
+        String sql = "SELECT t.transfer_id, t.transfer_type_id, t.transfer_status_id, t.account_from, t.account_to, t.amount " +
+                     "FROM transfers t " +
+                     "JOIN accounts a ON t.account_from = a.account_id OR t.account_to = a.account_id " +
+                     "WHERE a.user_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+        while(results.next()) {
+            Transfer transfer = mapRowToTransfer(results);
+            transfers.add(transfer);
+        }
+        return transfers;
+    }
 
-    List<Transfer> getPendingTransfers(Integer userId);
+    @Override
+    public Transfer getTransferById(Integer userId, Integer transferId) {
+        String sql = "SELECT t.transfer_id, t.transfer_type_id, t.transfer_status_id, t.account_from, t.account_to, t.amount " +
+                     "FROM transfers t " +
+                     "JOIN accounts a ON t.account_from = a.account_id OR t.account_to = a.account_id " +
+                     "WHERE a.user_id = ? AND t.transfer_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId, transferId);
+        if(results.next()) {
+            return mapRowToTransfer(results);
+        }
+        return null;
+    }
 
-    void updateTransferStatus(Integer transferId, Integer statusId);
+    @Override
+    public Transfer createTransfer(Integer userId, Transfer transfer) {
+        String sql = "INSERT INTO transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) " +
+                     "VALUES (?, ?, ?, ?, ?) RETURNING transfer_id";
+        Integer newId = jdbcTemplate.queryForObject(sql, Integer.class,
+                transfer.getTransferTypeId(),
+                transfer.getTransferStatusId(),
+                transfer.getAccountFrom(),
+                transfer.getAccountTo(),
+                transfer.getAmount());
+        transfer.setTransferId(newId);
+        return transfer;
+    }
+
+    @Override
+    public List<Transfer> getPendingTransfers(Integer userId) {
+        List<Transfer> transfers = new ArrayList<>();
+        String sql = "SELECT t.transfer_id, t.transfer_type_id, t.transfer_status_id, t.account_from, t.account_to, t.amount " +
+                     "FROM transfers t " +
+                     "JOIN accounts a ON t.account_from = a.account_id OR t.account_to = a.account_id " +
+                     "WHERE a.user_id = ? AND t.transfer_status_id = 1";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+        while(results.next()) {
+            Transfer transfer = mapRowToTransfer(results);
+            transfers.add(transfer);
+        }
+        return transfers;
+    }
+
+    @Override
+    public void updateTransferStatus(Integer transferId, Integer status```java
+Id) {
+        String sql = "UPDATE transfers SET transfer_status_id = ? WHERE transfer_id = ?";
+        jdbcTemplate.update(sql, statusId, transferId);
+    }
+
+    private Transfer mapRowToTransfer(SqlRowSet rs) {
+        Transfer transfer = new Transfer();
+        transfer.setTransferId(rs.getInt("transfer_id"));
+        transfer.setTransferTypeId(rs.getInt("transfer_type_id"));
+        transfer.setTransferStatusId(rs.getInt("transfer_status_id"));
+        transfer.setAccountFrom(rs.getInt("account_from"));
+        transfer.setAccountTo(rs.getInt("account_to"));
+        transfer.setAmount(rs.getDouble("amount"));
+        return transfer;
+    }
 }
