8a730afe8246e5c2984d31206b9a1111575a7d0d
Maxim Kerkula <87918161+MaxKerkula@users.noreply.github.com>
2023-06-09 02:48:54 -0400

    Update TransferDao.java
    Removed redundant logic and methods

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,25 +1,18 @@
 package com.techelevator.tenmo.dao;
 
-import com.techelevator.tenmo.model.*;
+import com.techelevator.tenmo.model.Transfer;
 
 import java.util.List;
 
-public interface TransferDao {
+public interface TransferDAO {
 
     List<Transfer> getAllTransfers(Integer userId);
-    
-    List<Transfer> getTransfersByUserId(int userId);
-    
+
     Transfer getTransferById(Integer userId, Integer transferId);
 
     Transfer createTransfer(Integer userId, Transfer transfer);
 
     List<Transfer> getPendingTransfers(Integer userId);
 
-    void updateTransferStatus(Integer transferId, TransferStatus status);
-    String getAccountFromUsername(Integer userId);
-
-    String getTransferTypeById(Integer transferTypeId);
-    String getTransferStatusById(Integer transferStatusId);
-
+    void updateTransferStatus(Integer transferId, Integer statusId);
 }
